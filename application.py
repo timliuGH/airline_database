bd7a57681fc2b57455bcaddde1dd1831d192eb1e
timliuGH <timsgithub@gmail.com>
2019-08-27 09:10:53 -0700

    Add book route

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
@@ -13,3 +13,23 @@ db = scoped_session(sessionmaker(bind=engine))
 def index():
     flights = db.execute("SELECT * FROM flights").fetchall()
     return render_template("index.html", flights=flights)
+
+@app.route("/book", methods=["POST"])
+def book():
+    """Book a flight with passenger's name"""
+    # Get passenger's name
+    passenger_name = request.form.get("passenger_name")
+
+    # Get flight selected by passenger
+    flight_id = request.form.get("flight_id")
+
+    flight_info = db.execute("SELECT origin, destination FROM flights \
+        WHERE id = :flight_id", {"flight_id": flight_id}).fetchone()
+
+    db.execute("INSERT INTO passengers (name, flight_id) \
+        VALUES (:name, :flight_id)",
+        {"name": passenger_name, "flight_id": flight_id})
+    db.commit()
+
+    return render_template("success.html", origin=flight_info[0],
+        destination=flight_info[1], passenger=passenger_name)
