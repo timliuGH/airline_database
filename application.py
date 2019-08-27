fcee8604c5e19283dcddb9125b35ecdb23b891a1
timliuGH <timsgithub@gmail.com>
2019-08-26 17:47:10 -0700

    Add dropdown for flights

---
+++ application.py
@@ -0,0 +1,15 @@
+import os
+
+from flask import Flask, render_template
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+app = Flask(__name__)
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+@app.route("/")
+def index():
+    flights = db.execute("SELECT * FROM flights").fetchall()
+    return render_template("index.html", flights=flights)
